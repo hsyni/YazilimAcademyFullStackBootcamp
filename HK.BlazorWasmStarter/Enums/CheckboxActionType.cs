eb1077a64e0ffbe64c65ffe1080b6968648aff66
Hüseyin Özgür Kazan <huseyinozgurkazan@outlook.com>
2024-07-11 08:20:29 +0300

    PasswordGenerator is in progress

---
+++ HK.BlazorWasmStarter/Enums/CheckboxActionType.cs
@@ -0,0 +1,10 @@
+﻿namespace HK.BlazorWasmStarter.Enums
+{
+    public enum CheckboxActionType
+    {
+        Numbers = 1,
+        Lowercase = 2,
+        Uppercase = 3,
+        SpecialChars = 4
+    }
+}
